8271d210f9bffbeb1cb90069899001fe60ca0df4
Chislaine <chislaine.cruz@gmail.com>
2022-09-03 16:14:32 -0400

    add db

--- src/pouchdb.js
+++ src/pouchdb.js
@@ -1,9 +1,6 @@
 const PouchDB = require("pouchdb");
 const pkg = require("../package.json");
 
-const dbName =
-  process.env.NODE_ENV === "production" ? pkg.name : `${pkg.name}_test`;
-
-const db = new PouchDB(dbName, { auto_compaction: true });
+const db = new PouchDB(pkg.name, { auto_compaction: true });
 
 module.exports = db;
